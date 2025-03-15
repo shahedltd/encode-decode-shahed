1cfdcf09599d2e15c6b52656b7df8a810a260ad8
shahedltd <153431638+shahedltd@users.noreply.github.com>
2025-03-15 13:55:11 +0600

    Update encode_decode.py because it's give an error so don't use this file in your IDE!

--- encode_decode.py
+++ encode_decode.py
@@ -1,12 +1,1 @@
-from encode_decode.encoder_decoder import encode_text, decode_text
-
-# Your data to encode and decode
-original_data = "Hello World"
-
-# Encoding the data
-encoded_data = encode_text(original_data)
-print(f"Encoded Data: {encoded_data}")
-
-# Decoding the data
-decoded_data = decode_text(encoded_data)
-print(f"Decoded Data: {decoded_data}")
+## It's give you an error so don't use this file in your IDE!
